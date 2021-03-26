898f8d15bc0ff7ba9e98aaf4021910e417c3e64f
Dawg4321 <ryan.wiebe@outlook.com>
2021-03-26 22:49:47 +0000

    Performance increase within image inversion

--- imageinverter.py
+++ imageinverter.py
@@ -1,5 +1,5 @@
-# image_Inverter
-# This is a simple application which will invert the colours in any png and jpg image.
+# imageinverter.py
+# This is a simple application which will invert the colours in any .png and .jpg image.
 
 from PIL import Image
 import numpy as np
@@ -30,26 +30,23 @@ print(divider, "\nInverting Image...")
 
 selected_image = images[int(selected_image) - 1] # locating the selected image in the images list
 
-## **IMAGE INVERSION**
+## **IMAGE PREPARATION**
 im = Image.open(selected_image) # opening the image for processing
 
-im_size = im.size # size of image in pixels
+im = im.convert(mode="RGBA") # converting image format to RGBA before processing
 
-im = im.convert(mode="RGBA") # removing transparency from any RGBA images by converting them to RGB
+## **IMAGE INVERSION**
+pixelmap = np.asarray(im) # creating a pixel map of the selected image in an array
+                          # pixelmap = [[R0, G0, B0, A0] [R1, G1, B1, A1]... [Rn-1, Gn-1, Bn-1, An-1]]
 
-pixelmap = np.asarray(im.getdata()) # creating a pixel map of the selected image in an array
-                                    # pixelmap = [[R0, G0, B0, A0] [R1, G1, B1, A1]... [Rn-1, Gn-1, Bn-1, An-1]]
+pixelmap = [255,255,255,0] - pixelmap.reshape(-1,4) # inverting each colour by subtracting them from 255 (each colour has 1 byte or 8 bits of data)
+                                                    # transparency byte is subtracted from 0
+                                                    # pixelmap = [[255-R0, 255-G0, 255-B0, 0-A0] [255-R1, 255-G1, 255-B1, 0-A1]... [255-Rn-1, 255-Gn-1, 255-Bn-1, 0-An-1]]
 
-pixelmap_copy = np.delete(pixelmap,3,1) # creating a copy of the pixel map without the transparency data 
-                                        # pixelmap = [[R0, G0, B0] [R1, G1, B1]... [Rn-1, Gn-1, Bn-1]]
+pixelmap = pixelmap * [1,1,1,-1] # readjusting transparent byte to original non-negative value
+                                 # pixelmap = [[255-R0, 255-G0, 255-B0, A0] [255-R1, 255-G1, 255-B1, A1]... [255-Rn-1, 255-Gn-1, 255-Bn-1, An-1]]
 
-pixelmap_copy = 255 - pixelmap_copy # inverting each colour by subtracting them from 255 (each colour has 1 byte or 8 bits of data)
-                                    # pixelmap = [[255-R0, 255-G0, 255-B0] [255-R1, 255-G1, 255-B1]... [255-Rn-1, 255-Gn-1, 255-Bn-1]]
-
-pixelmap[:,0:3] = pixelmap_copy # merging the arrays together so that the editted colour data is replacing the old colour data while maintaining the transparency
-                                # pixelmap = [[255-R0, 255-G0, 255-B0, A0] [255-R1, 255-G1, 255-B1, A1]... [255-Rn-1, 255-Gn-1, 255-Bn-1, An-1]]
-
-im.putdata(tuple(map(tuple, pixelmap))) # mapping the 2d array to tuple so the pillow .putdata function can be used
+im.putdata(tuple(map(tuple, pixelmap))) # mapping the 2d array to tuple so the .putdata function can be used
 
 ## **IMAGE SAVING** 
 selected_image = selected_image.replace(".png","") # removing any labels at the end of the name before saving 
@@ -58,4 +55,4 @@ selected_image = selected_image.replace(".jpeg","")
 
 im.save(selected_image + "_Inverted.png") # saving inverted image
 
-print(divider, "\nImage has successfully been inverted. A copy of this has been saved in ", currpath)
+print(divider, "\nImage has successfully been inverted. A copy of this has been saved in", currpath)
