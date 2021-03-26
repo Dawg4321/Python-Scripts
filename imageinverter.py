864208116119b63cdd06d70767c8ed9d8c84b8cb
Dawg4321 <ryan.wiebe@outlook.com>
2021-03-26 14:14:03 +0000

    imageinverter now keeps transparency in RGBA images

--- imageinverter.py
+++ imageinverter.py
@@ -32,15 +32,24 @@ selected_image = images[int(selected_image) - 1] # locating the selected image i
 
 ## **IMAGE INVERSION**
 im = Image.open(selected_image) # opening the image for processing
-im = im.convert(mode="RGB") # removing transparency from any RGBA images by converting them to RGB
 
-pixelmap = np.asarray(im) # creating a pixel map of the selected image in an array [R0, G0, B0, R1, G1, B1... Rn-1, Gn-1, Bn-1]
+im_size = im.size # size of image in pixels
 
-pixelmap = 255 - pixelmap # inverting each colour by subtracting them from 255 (each colour has 1 byte or 8 bits of data)
+im = im.convert(mode="RGBA") # removing transparency from any RGBA images by converting them to RGB
 
-im = Image.fromarray(pixelmap) # creating new image with modified pixel map
+pixelmap = np.asarray(im.getdata()) # creating a pixel map of the selected image in an array
+                                    # pixelmap = [[R0, G0, B0, A0] [R1, G1, B1, A1]... [Rn-1, Gn-1, Bn-1, An-1]]
 
-im.show() # displaying inverted image
+pixelmap_copy = np.delete(pixelmap,3,1) # creating a copy of the pixel map without the transparency data 
+                                        # pixelmap = [[R0, G0, B0] [R1, G1, B1]... [Rn-1, Gn-1, Bn-1]]
+
+pixelmap_copy = 255 - pixelmap_copy # inverting each colour by subtracting them from 255 (each colour has 1 byte or 8 bits of data)
+                                    # pixelmap = [[255-R0, 255-G0, 255-B0] [255-R1, 255-G1, 255-B1]... [255-Rn-1, 255-Gn-1, 255-Bn-1]]
+
+pixelmap[:,0:3] = pixelmap_copy # merging the arrays together so that the editted colour data is replacing the old colour data while maintaining the transparency
+                                # pixelmap = [[255-R0, 255-G0, 255-B0, A0] [255-R1, 255-G1, 255-B1, A1]... [255-Rn-1, 255-Gn-1, 255-Bn-1, An-1]]
+
+im.putdata(tuple(map(tuple, pixelmap))) # mapping the 2d array to tuple so the pillow .putdata function can be used
 
 ## **IMAGE SAVING** 
 selected_image = selected_image.replace(".png","") # removing any labels at the end of the name before saving 
